0474c310ac3308c2b9baa0a8a6d410fbc6e76284
zhanzxy5 <zhanxianyuan@gmail.com>
2017-10-27 17:03:20 +0800

    Simple policy gradient method implementing REINFORCE algorithm

--- tf_drl_test.py
+++ tf_drl_test.py
@@ -139,11 +139,205 @@ def drl_test2():
     env.close()
 
 
+# Deep Q-learning with Atari Ms. Pac-Man game
+# Preprocessing the image
+mspacman_color = np.array([210, 164, 74]).mean()
+
+def preprocess_observation(obs):
+    img = obs[1:176:2, ::2]
+    img = img.mean(axis=2)
+    img[img == mspacman_color] = 0  # improve the contrast
+    img = (img - 128) / 128 - 1  # from -1 to 1
+    return img.reshape(88, 80, 1)
+
+def drl_test3():
+    from tensorflow.contrib.layers import convolution2d, fully_connected
+    from collections import deque
+    import os
+
+    # Set-up the Atari environment
+    env = gym.make("MsPacman-v9")
+
+    # Parameters
+    input_height = 88
+    input_width = 80
+    input_channels = 1
+    conv_n_maps = [32, 64, 64]
+    conv_kernel_sizes = [(8, 8), (4, 4), (3, 3)]
+    conv_strides = [4, 2, 1]
+    conv_paddings = ["SAME"] * 3
+    conv_activation = [tf.nn.relu] * 3
+    n_hidden_in = 64 * 11 * 10
+    n_hidden = 512
+    hidden_activation = tf.nn.relu
+    n_outputs = env.action_space.n
+    initializer = tf.contrib.layers.variance_scaling_initializer()
+
+    learning_rate = 0.01
+
+    # Used to create two Q networks, one is to provide temporary policy, the other perform optimization (double Q-learning)
+    def q_network(X_state, scope):
+        prev_layer = X_state
+        conv_layers = []
+        # Build all layers using a loop
+        with tf.variable_scope(scope) as scope:
+            for n_maps, kernel_size, stride, padding, activation in zip(conv_n_maps, conv_kernel_sizes, conv_strides, conv_paddings, conv_activation):
+                prev_layer = convolution2d(prev_layer, num_outputs=n_maps, kernel_size=kernel_size, stride=stride,
+                                           padding=padding, activation_fn=activation, weights_initializer=initializer)
+                conv_layers.append(prev_layer)
+            last_conv_layer_flat = tf.reshape(prev_layer, shape=[-1, n_hidden_in])
+            hidden = fully_connected(last_conv_layer_flat, num_outputs=n_hidden, activation_fn=hidden_activation, weights_initializer=initializer)
+            outputs = fully_connected(hidden, num_outputs=n_outputs, activation_fn=None, weights_initializer=initializer)
+        trainable_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope.name)
+        # Name to variable map, strip out the scope name
+        trainable_vars_by_name = {var.name[len(scope.name):]: var for var in trainable_vars}
+
+        return outputs, trainable_vars_by_name
+
+    # Construct the model
+    X_state = tf.placeholder(tf.float32, shape=[None, input_height, input_width, input_channels])
+    # The one who provide temporary policy (online)
+    online_q_values, online_vars = q_network(X_state, "q_network/online")
+    # The one perform optimization (target)
+    target_q_values, target_vars = q_network(X_state, "q_network/target")
+
+    # Periodically copy optimized values (target) to the temporary Q-network (online)
+    copy_ops = [target_var.assign(online_vars[var_name]) for var_name, target_var in target_vars.items()]
+    copy_online_to_target = tf.group(*copy_ops) # group all copy operation to a single operation
+
+    learning_rate = 0.001
+    momentum = 0.95
+
+    with tf.variable_scope("train"):
+        X_action = tf.placeholder(tf.int32, shape=[None])
+        y = tf.placeholder(tf.float32, shape=[None, 1])
+        q_value = tf.reduce_sum(online_q_values * tf.one_hot(X_action, n_outputs),
+                                axis=1, keep_dims=True)
+        # Tweak to make the error being a squared error at [0,1] plus the linear part if greater than 1.0
+        error = tf.abs(y - q_value)
+        clipped_error = tf.clip_by_value(error, 0.0, 1.0)
+        linear_error = 2 * (error - clipped_error) # times 2 is to make the gradient continuous
+        loss = tf.reduce_mean(tf.square(clipped_error) + linear_error)
+
+        global_step = tf.Variable(0, trainable=False, name='global_step')
+        optimizer = tf.train.MomentumOptimizer(learning_rate, momentum, use_nesterov=True)
+        training_op = optimizer.minimize(loss, global_step=global_step)
+
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
 
 
+    # Set up the replay buffer
+    replay_memory_size = 10000
+    replay_memory = deque([], maxlen=replay_memory_size)
+
+    def sample_memories(batch_size):
+        indices = np.random.permutation(len(replay_memory))[:batch_size]
+        cols = [[], [], [], [], []] # state, action, reward, next_state, continue
+        for idx in indices:
+            memory = replay_memory[idx]
+            # Interesting implementation
+            for col, value in zip(cols, memory):
+                col.append(value)
+        cols = [np.array(col) for col in cols] # Convert to numpy array
+        return cols[0], cols[1], cols[2].reshape(-1, 1), cols[3], cols[4].reshape(-1, 1)
+
+    # Set-up epsilon-greedy policy: gradually reduce from 1 to 0.05 in 50,000 training steps
+    eps_min = 0.05
+    eps_max = 1.0
+    eps_decay_step = 50000
+
+    def epsilon_greedy(q_values, step):
+        epsilon = max(eps_min, eps_max - (eps_max - eps_min) * step / eps_decay_step)
+        if np.random.rand() < epsilon:
+            return np.random.randint(n_outputs)
+        else:
+            return np.argmax(q_values) # optimal action
+
+    # Training section
+    # Parameters
+    n_steps = 100000        # N training steps
+    training_start = 100    # start training after 1000 game iterations
+    training_interval = 3   # run training step every 3 game iterations
+    save_steps = 100        # save every some steps
+    copy_steps = 25         # copy target to online network period
+    discount_rate = 0.95
+    skip_start = 90         # skip the start of every game
+    batch_size = 50
+    iteration = 0           # game iteration
+    checkpoint_path = "data/DQN"
+    done = True             # env needs to be reset
+
+    # Parameters to track the game performance
+    loss_val = np.infty
+    game_length = 0
+    total_max_q = 0
+    mean_max_q = 0.0
+
+    with tf.Session() as sess:
+        if os.path.isfile(checkpoint_path + ".index"):
+            saver.restore(sess, checkpoint_path)
+        else:
+            init.run()
+            copy_online_to_target.run()
+        while True:
+            step = global_step.eval()
+            if step >= n_steps:
+                break
+            iteration += 1
+            print("\rIteration {}\tTraining step {}/{} ({:.1f})%\tLoss {:5f}\tMean Max-Q {:5f}   ".format(
+                iteration, step, n_steps, step * 100 / n_steps, loss_val, mean_max_q), end="")
+
+            if done: # if game over, start again
+                obs = env.reset()
+                # skip the start of each game
+                for skip in range(skip_start):
+                    obs, reward, done, info = env.step(0)
+                state = preprocess_observation(obs)
+
+            # Online step
+            q_value = online_q_values.eval(feed_dict={X_state: state})
+            action = epsilon_greedy(q_value, step)
+
+            # Play
+            obs, reward, done, info = env.step(action)
+            next_state = preprocess_observation(obs)
+
+            # Store memory
+            replay_memory.append((state, action, reward, next_state, 1.0 - done))
+            state = next_state
+
+            # Compute statistics for tracking progress
+            total_max_q += q_value.max()
+            game_length += 1
+            if done:
+                mean_max_q = total_max_q / game_length
+                total_max_q = 0.0
+                game_length = 0
+
+            if iteration < training_start or iteration % training_interval != 0:
+                continue
+
+            # Sample memories and use the target DQN to produce the target Q-Value
+            X_state_val, X_action_val, rewards, X_next_state_val, continues = (sample_memories(batch_size))
+            next_q_values = target_q_values.eval(feed_dict={X_state: X_next_state_val})
+            max_next_q_values = np.max(next_q_values, axis=1, keepdims=True)
+            y_val = rewards + continues * discount_rate * max_next_q_values
+
+            # Train the online DQN
+            _, loss_val = sess.run([training_op, loss], feed_dict={
+                X_state: X_state_val, X_action: X_action_val, y: y_val})
+
+            # Regularly copy the online DQN to the target DQN
+            if step % copy_steps == 0:
+                copy_online_to_target.run()
+
+            # And save regularly
+            if step % save_steps == 0:
+                saver.save(sess, checkpoint_path)
 
 def main():
-    drl_test2()
+    drl_test3()
 
 if __name__ == "__main__":
     main()
